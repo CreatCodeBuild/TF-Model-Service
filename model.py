a85559af83d0d93603a2256357c853fad2b229cc
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2016-12-26 05:01:22 -0500

    + 10 digits recognized

--- model.py
+++ model.py
@@ -5,17 +5,17 @@ import cv2 as cv
 class DefaultModelServer():
 	def __init__(self):
 		self.sess = tf.Session()
+		self.new_saver = tf.train.import_meta_graph('model/default.ckpt.meta')
+		self.new_saver.restore(self.sess, 'model/default.ckpt')
+		self.test_prediction = tf.get_default_graph().get_tensor_by_name('test/single_prediction:0')
+		self.single_input = tf.get_default_graph().get_tensor_by_name('test/single_input:0')
 
 	def compute(self, inputData):
 		'''
 		:param inputData: a np array with shape (1, 32, 32, 1), representing 1 grayscaled image
 		:return: the softmaxed result
 		'''
-		new_saver = tf.train.import_meta_graph('model/default.ckpt.meta')
-		new_saver.restore(self.sess, 'model/default.ckpt')
-		test_prediction = tf.get_default_graph().get_tensor_by_name('test/single_prediction:0')
-		single_input = tf.get_default_graph().get_tensor_by_name('test/single_input:0')
-		return self.sess.run(test_prediction, feed_dict={single_input: inputData})
+		return self.sess.run(self.test_prediction, feed_dict={self.single_input: inputData})
 
 	def transform_data(self, image):
 		# the shape of image should be (32, 32, 1)
@@ -27,12 +27,12 @@ class DefaultModelServer():
 	def serve(self, image):
 		return self.compute(self.transform_data(image))
 
-server = DefaultModelServer()
 
-from PIL import Image
-
-for i in range(2):
-	im = cv.imread(str(i) + ".png", cv.IMREAD_COLOR)
-	result = server.serve(im)
-	print(result)
+# Test
+if __name__ == '__main__':
+	server = DefaultModelServer()
+	for i in range(10):
+		im = cv.imread(str(i) + ".png", cv.IMREAD_COLOR)
+		result = server.serve(im)
+		print(result)
 
