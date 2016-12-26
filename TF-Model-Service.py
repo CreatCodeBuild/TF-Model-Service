2c8a39b54257562a99bd2c2f87567cedc46eb01a
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2016-12-26 05:36:05 -0500

    + /single-digit [implemented, need to improve]

--- TF-Model-Service.py
+++ TF-Model-Service.py
@@ -1,15 +1,22 @@
-from flask import Flask
+from flask import Flask, request
+import model
+import cv2 as cv
+import numpy as np
 
 app = Flask(__name__)
 
-
-def read_model():
-	pass
-
+server = model.DefaultModelServer()
 
 @app.route('/')
 def hello_world():
 	return 'Fuck Yo!'
+
+
+@app.route('/single-digit', methods=['POST'])
+def single_digit():
+	print('data length', len(request.data))
+	q = cv.imdecode(np.frombuffer(request.data, dtype='uint8'), cv.IMREAD_COLOR)
+	return str(server.serve(q))
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -5,19 +5,20 @@ import cv2 as cv
 class DefaultModelServer():
 	def __init__(self):
 		self.sess = tf.Session()
-		self.new_saver = tf.train.import_meta_graph('model/default.ckpt.meta')
-		self.new_saver.restore(self.sess, 'model/default.ckpt')
+		self.new_saver = tf.train.import_meta_graph('E:\Git Repo\TF-Model-Service\model\default.ckpt.meta')
+		self.new_saver.restore(self.sess, 'E:\Git Repo\TF-Model-Service\model\default.ckpt')
 		self.test_prediction = tf.get_default_graph().get_tensor_by_name('test/single_prediction:0')
 		self.single_input = tf.get_default_graph().get_tensor_by_name('test/single_input:0')
 
 	def compute(self, inputData):
 		'''
-		:param inputData: a np array with shape (1, 32, 32, 1), representing 1 grayscaled image
+		:param inputData: a ndarray with shape (1, 32, 32, 1), representing 1 grayscaled image
 		:return: the softmaxed result
 		'''
 		return self.sess.run(self.test_prediction, feed_dict={self.single_input: inputData})
 
 	def transform_data(self, image):
+		# :param image: ndarray
 		# the shape of image should be (32, 32, 1)
 		if image.shape[2] != 1:
 			image = tf.image.rgb_to_grayscale(image)
@@ -34,5 +35,5 @@ if __name__ == '__main__':
 	for i in range(10):
 		im = cv.imread(str(i) + ".png", cv.IMREAD_COLOR)
 		result = server.serve(im)
-		print(result)
+		print(str(result))
 
