e16246f294383315353027127ac3d531075a2f89
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2016-12-26 04:03:41 -0500

    + Recognized 1

--- model.py
+++ model.py
@@ -1,21 +1,40 @@
 import tensorflow as tf
 import numpy as np
 
-samples = np.zeros(shape=(1, 32, 32, 1))
+class DefaultModelServer():
+	def __init__(self):
+		self.sess = tf.Session()
 
-sess = tf.Session()
-new_saver = tf.train.import_meta_graph('model/default.ckpt.meta')
-new_saver.restore(sess, 'model/default.ckpt')
-# tf.get_collection() returns a list. In this example we only want the
-# first one.
-# test = tf.get_default_graph().get_all_collection_keys()
-# print(tf.get_default_graph().get_collection('prediction'))
-test_prediction = tf.get_default_graph().get_tensor_by_name('test/single_prediction:0')
-single_input = tf.get_default_graph().get_tensor_by_name('test/single_input:0')
+	def compute(self, inputData):
+		'''
+		:param inputData: a np array with shape (1, 32, 32, 1), representing 1 grayscaled image
+		:return: the softmaxed result
+		'''
+		new_saver = tf.train.import_meta_graph('model/default.ckpt.meta')
+		new_saver.restore(self.sess, 'model/default.ckpt')
+		test_prediction = tf.get_default_graph().get_tensor_by_name('test/single_prediction:0')
+		single_input = tf.get_default_graph().get_tensor_by_name('test/single_input:0')
+		return self.sess.run(test_prediction, feed_dict={single_input: inputData})
 
-for i in range(10):
-	result = sess.run(
-		test_prediction,
-		feed_dict={single_input: samples}
-	)
-	print(result)
+	def transform_data(self, image):
+		# the shape of image should be (32, 32, 1)
+		if image.shape[2] != 1:
+			image = tf.image.rgb_to_grayscale(image)
+		image = tf.image.resize_images(image, (32, 32))
+		return [image.eval(session=self.sess)]
+
+	def serve(self, image):
+		return self.compute(self.transform_data(image))
+
+server = DefaultModelServer()
+
+from PIL import Image
+im = Image.open("1.png")
+
+
+
+image = np.array(im.getdata(), np.float32).reshape(im.size[1], im.size[0], 3)
+#print(type(image), image.shape)
+result = server.serve(image)
+print(result)
+
